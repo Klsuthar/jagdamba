36505988a24327f91282f5f4ed33bc6fc86f78b7
KhatiRaj <93217940+Klsuthar@users.noreply.github.com>
2025-11-05 22:11:48 +0530

    hii

--- js/gallery.js
+++ js/gallery.js
@@ -7,7 +7,17 @@ const galleryData = [
     { thumb: 'images/Director.jpg', full: 'images/Director.jpg', alt: 'निदेशक' },
     { thumb: 'images/Partibha_smman.jpg', full: 'images/Partibha_smman.jpg', alt: 'प्रतिभा सम्मान' },
     { thumb: 'images/Partibha_smman2.jpg', full: 'images/Partibha_smman2.jpg', alt: 'प्रतिभा सम्मान 2' },
-    { thumb: 'images/Partibha_smman3.jpg', full: 'images/Partibha_smman3.jpg', alt: 'प्रतिभा सम्मान 3' }
+    { thumb: 'images/Partibha_smman3.jpg', full: 'images/Partibha_smman3.jpg', alt: 'प्रतिभा सम्मान 3' },
+    { thumb: 'images/events/drawing_competition/drawing_competition (1).jpg', full: 'images/events/drawing_competition/drawing_competition (1).jpg', alt: 'चित्रकला प्रतियोगिता 1' },
+    { thumb: 'images/events/drawing_competition/drawing_competition (2).jpg', full: 'images/events/drawing_competition/drawing_competition (2).jpg', alt: 'चित्रकला प्रतियोगिता 2' },
+    { thumb: 'images/events/drawing_competition/drawing_competition (3).jpg', full: 'images/events/drawing_competition/drawing_competition (3).jpg', alt: 'चित्रकला प्रतियोगिता 3' },
+    { thumb: 'images/events/drawing_competition/drawing_competition (4).jpg', full: 'images/events/drawing_competition/drawing_competition (4).jpg', alt: 'चित्रकला प्रतियोगिता 4' },
+    { thumb: 'images/events/drawing_competition/drawing_competition (5).jpg', full: 'images/events/drawing_competition/drawing_competition (5).jpg', alt: 'चित्रकला प्रतियोगिता 5' },
+    { thumb: 'images/events/drawing_competition/drawing_competition (6).jpg', full: 'images/events/drawing_competition/drawing_competition (6).jpg', alt: 'चित्रकला प्रतियोगिता 6' },
+    { thumb: 'images/events/drawing_competition/drawing_competition (7).jpg', full: 'images/events/drawing_competition/drawing_competition (7).jpg', alt: 'चित्रकला प्रतियोगिता 7' },
+    { thumb: 'images/events/drawing_competition/drawing_competition (8).jpg', full: 'images/events/drawing_competition/drawing_competition (8).jpg', alt: 'चित्रकला प्रतियोगिता 8' },
+    { thumb: 'images/events/drawing_competition/drawing_competition (9).jpg', full: 'images/events/drawing_competition/drawing_competition (9).jpg', alt: 'चित्रकला प्रतियोगिता 9' },
+    { thumb: 'images/events/drawing_competition/drawing_competition (10).jpg', full: 'images/events/drawing_competition/drawing_competition (10).jpg', alt: 'चित्रकला प्रतियोगिता 10' }
 ];
 
 galleryImages = galleryData.map(item => item.full);
